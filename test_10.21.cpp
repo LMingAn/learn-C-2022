2259b5bfb215c5068083d3475df189a23fdf4961
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-10-22 09:23:43 +0800

    Add files via upload

---
+++ test_10.21.cpp
@@ -0,0 +1,50 @@
+﻿//#include<stdio.h>
+//int main()
+//{
+//	int x, n, z, e;
+//	scanf_s("%d %d", &x, &n);
+//	int arr[100];
+//	for (z = 0; z < x; z++)
+//	{
+//		scanf_s("%d", &arr[z]);
+//	}
+//	for (z = 0; z < x; z++)
+//	{
+//		for (int q = z + 1; q < x; q++)
+//		{
+//			if (arr[z] > arr[q])
+//			{
+//				e = arr[z];
+//				arr[z] = arr[q];
+//				arr[q] = e;
+//			}
+//		}//或许只需要处理删除后的数组内容
+//		if (arr[z] % n != 0)
+//		{
+//			if (arr[z] >= 65 && arr[z] <= 90)
+//				printf("%c ", arr[z]);
+//			else
+//				printf("%d ", arr[z]);
+//		}
+//	}
+//	return 0;
+//}
+
+
+#include<stdio.h>
+int fun(int x)
+{
+	if (x == 1 || x == 2)
+		return 1;
+	else if (x == 0)return 0;
+	x = fun(x - 1) + fun(x - 2);
+	return x;
+}
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	printf("%d", fun(n));
+	return 0;
+}
+
