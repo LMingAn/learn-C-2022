47468e8995be0f7524086811309e5a5ee4906ab1
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-10-25 23:24:37 +0800

    Add files via upload

---
+++ test_10.25.cpp
@@ -0,0 +1,28 @@
+﻿#include<stdio.h>
+int main()
+{
+    int a;
+    scanf_s("%d", &a);
+    float x = 0, b = 0, c = 0, d = 0;
+    if (a >= 0 && a <= 150)
+    {
+        x = a * 0.4463;
+        printf("%.1f", x);
+    }
+    else if (a > 150 && a <= 400)
+    {
+        b = 150 * 0.4463;
+        c = (a - 150) * 0.4663;
+        x = b + c;
+        printf("%.1f", x);
+    }
+    else
+    {
+        b = 150 * 0.4463;
+        c = 250 * 0.4663;
+        d = (a - 400) * 0.5663;
+        x = b + c + d;
+        printf("%.1f", x);
+    }
+    return 0;
+}
