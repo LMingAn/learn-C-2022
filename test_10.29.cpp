7b8451d7e54a5f0c53815d2f17ab1327264bb9c6
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-10-29 11:31:10 +0800

    Add files via upload

---
+++ test_10.29.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+    float x, a = 0, b;
+    int c = 0;
+    scanf_s("%f", &x);
+    for (b = 2; b >= 0;)
+    {
+        a = a + b;
+        b = b * 0.98;
+        c++;
+        if (a >= x)
+        {
+            printf("%d", c);
+            break;
+        }
+    }
+    return 0;
+}
