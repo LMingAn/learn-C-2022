b74bd0bf5b346c4f11bff87c15fc5426b9506bd9
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-11-09 01:14:52 +0800

    Add files via upload

---
+++ test_11.6.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void main()
+{
+	double x = 1, y;
+	int a;
+	scanf_s("%d", &x);
+	a = (1 <= x && x < 2) ? 1 : (2 <= x && x <= 5) ? 2 : 3;
+	switch (a)
+	{
+	case 1:y = 2 * x + 5; break;
+	case 2:y = sqrt(1 + x * x); break;
+	}
+	printf("y=%f", y);
+	system("pause");
+}
---
+++ test_11.8.cpp
@@ -0,0 +1,54 @@
+﻿//猜数字
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+void menu()
+{
+	printf("************************************\n");
+	printf("*****  1.开始游戏  0.退出游戏  *****\n");
+	printf("************************************\n");
+}
+void game()
+{
+	int b, c;
+	b = rand();
+	printf("请猜数字：");
+	while (1)
+	{
+		scanf_s("%d", &c);
+		if (c > b)
+			printf("大了\n");
+		else if (c < b)
+			printf("小了\n");
+		else
+		{
+			printf("恭喜你，猜对了\n");
+			break;
+		}
+	}
+}
+int main()
+{
+	int a;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf_s("%d", &a);
+		switch (a)
+		{
+		case 0:
+			printf("退出游戏\n");
+			break;
+		case 1:
+			game();
+			break;
+		default:
+			printf("输入错误，请重试\n");
+			break;
+		}
+	} while (a);
+	return 0;
+}
