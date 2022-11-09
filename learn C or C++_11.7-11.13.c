32f50964dd6610c5525b88e530b57e649affab52
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-11-09 23:35:24 +0800

    Add files via upload

--- learn C or C++_11.7-11.13.c
+++ learn C or C++_11.7-11.13.c
@@ -123,24 +123,127 @@
 //	return 0;
 //}
 
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//int main()
+//{
+//	char a[20] = { 0 };
+//	//shutdown -s -t 60
+//	//system()- 执行系统命令的程序
+//	system("shutdown -s -t 60");
+//	while (1)
+//	{
+//		printf("请注意，你的电脑将在1分钟后关机，如果输入：我是猪，就取消关机\n请输入：");
+//		scanf_s("%s", a);
+//		if (strcmp(a, "我是猪") == 0)
+//		{
+//			system("shutdown -a");
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+
+//函数
+//基本的定义函数写法
+//#include<stdio.h>
+//int add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	int c = add(a, b);
+//	printf("%d", c);
+//	return 0;
+//}
+
+
+//strcpy数组拷贝函数
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char a1[] = "LXG";
+//	char a2[20] = "################";
+//	strcpy_s(a2, 4, a1);//这里使用strcpy会报错，提示不安全，如果使用strcpy_s需要指定拷贝长度，拷贝长度需要大于等于需要拷贝的数组长度（注：字符串数组包含\n）
+//	printf("%s\n", a2);
+//	return 0;
+//}
+
+
+//memset内存设置函数
+//#include<stdio.h>
+//int main()
+//{
+//	char a[] = "hello world";
+//	memset(a, '*', 5);//即将设定的数组中的指定的前X位字符更换为指定的字符
+//	printf("%s\n", a);
+//	return 0;
+//}
+
+
+//自定义函数-格式
+//ret_type fun_name(para1, * )
+//{
+//statement;//语句项
+//}
+//ret_type 返回类型
+//fun_name 函数名
+//para1    函数参数
+
+
+//自定义函数-找出两个函数中的最大值
+//#include<stdio.h>
+//int MAX(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf_s("%d %d", &a, &b);
+//	int max = MAX(a, b);
+//	printf("%d", max);
+//	return 0;
+//}
+
+
+//自定义函数-交换两数
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+//int each1(int x, int y)//因为此函数中的x与y是独立于主函数中的a和b
+//{
+//	int z = 0;//更改x与y不能对主函数中的a和b产生影响
+//	z = x;
+//	x = y;
+//	y = z;
+//	return x, y;
+//}
+each2(int* x, int* y)//运用指针进行变量地址的更改
+{
+	int z = 0;
+	z = *x;
+	*x = *y;
+	*y = z;
+}
 int main()
 {
-	char a[20] = { 0 };
-	//shutdown -s -t 60
-	//system()- 执行系统命令的程序
-	system("shutdown -s -t 60");
-	while (1)
-	{
-		printf("请注意，你的电脑将在1分钟后关机，如果输入：我是猪，就取消关机\n请输入：");
-		scanf_s("%s", a);
-		if (strcmp(a, "我是猪") == 0)
-		{
-			system("shutdown -a");
-			break;
-		}
-	}
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	printf("%d %d\n", a, b);
+	//each1(a, b);//此方法不能用于交换两数
+	each2(&a, &b);//将a与b的地址放入自定义函数中进行更改
+	printf("%d %d", a, b);
 	return 0;
 }
