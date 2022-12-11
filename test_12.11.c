42f762f49fc854d8e8be76581f42b90ef54ec518
MingAn <113867200+LMingAn@users.noreply.github.com>
2022-12-11 17:05:27 +0800

    Add files via upload

---
+++ test_12.11.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i, j, m = 0;
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 3; j > 0; j--)
+		{
+			if ((i * j) > 3)
+				break;
+			m = i * j;
+		}
+	}
+	printf("%d", m);
+}
