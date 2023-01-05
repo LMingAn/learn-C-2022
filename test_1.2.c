ac06764cba96631da566ef3d6e2b2ce0fa9e2636
MingAn <113867200+LMingAn@users.noreply.github.com>
2023-01-05 17:09:42 +0800

    Add files via upload

---
+++ test_1.2.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include   <stdio.h>
+#include   <string.h>
+int  fun(char  s[])
+
+{
+
+	char* p = s;
+
+	while (*p != 0)  p++;
+
+	return (p - s);
+
+}
+int main()
+{
+	static int a[] = { 0,1,2,3,4 }, * p;
+
+	p = &a[2];
+	(*p)++;
+	return 0;
+}
---
+++ test_1.3.c
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string.h>
+//void fun(char* s, char* t)
+//{
+//	char k;
+//	k = *s;
+//	*s = *t;
+//	*t = k;
+//	s++;
+//	t--;
+//	if (*s)
+//		fun(s, t);
+//}
+//int main()
+//{
+//	printf("%d\n", 'c' && 'd');
+//	printf("%d\n", 'c' && 'd' || !(3 + 4));
+//	return 0;
+//}
+#include<stdio.h>
+#define N 80
+int main()
+{
+    int i, j = 0, k = 0, flag;
+    int a[N] = { 2, 2, 2, 3, 4, 4, 5, 6, 6, 6, 6, 7, 7, 8, 9, 9, 10, 10, 10, 10, 10 };
+    for (i = 0; i < N; i++)
+    {
+        flag = 1;
+        for (j = i + 1; j < N; j++)
+        {
+            if (a[i] == a[j])
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+            a[k++] = a[i];
+    }
+    for (i = 0; i < k; i++)
+    {
+        printf("%d", a[i]);
+        if (i != k - 1)
+            printf(" ");
+    }
+    return 0;
+}
