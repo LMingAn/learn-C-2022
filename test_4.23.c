dba33b8c24c2381062749c321e9d761af7610a93
MingAn <113867200+LMingAn@users.noreply.github.com>
2023-04-23 22:49:33 +0800

    Add files via upload

---
+++ test_4.23.c
@@ -0,0 +1,42 @@
+﻿#include<stdio.h> 
+#include<string.h>
+
+void reverse(char* left, char* right)
+{
+	while (left < right)
+	{
+		char a = *left;
+		*left = *right;
+		*right = a;
+		left++;
+		right--;
+	}
+}
+
+int main()
+{
+	char a[101] = { 0 };
+	//输入
+	gets(a);
+
+	//逆置
+	int len = strlen(a);
+	reverse(a, a + len - 1);
+
+	//逆序
+	char* start = a;
+	while (*start)
+	{
+		char* end = start;
+		while (*end != ' ' && *end != '\0')
+			end++;
+		reverse(start, end - 1);
+		if (*end != '\0')
+			end++;
+		start = end;
+	}
+
+	//输出
+	printf("%s", a);
+	return 0;
+}
